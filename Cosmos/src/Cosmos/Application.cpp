bc4b9fb87634a669293e34e2b24d541796fce2f8
Martin Gritsch <thegritsch@gmail.com>
2022-03-29 14:32:39 +0200

    Added window events and callbacks

--- Cosmos/src/Cosmos/Application.cpp
+++ Cosmos/src/Cosmos/Application.cpp
@@ -1,17 +1,34 @@
 #include "cmpch.h"
 #include "Application.h"
-#include "Cosmos/Events/ApplicationEvent.h"
+
 #include "Cosmos/Log.h"
 #include <GLFW/glfw3.h>
 
 namespace Cosmos {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Cosmos::Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Cosmos::Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		CS_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 	void Application::Run()
--- Cosmos/src/Cosmos/Application.h
+++ Cosmos/src/Cosmos/Application.h
@@ -2,6 +2,7 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "Cosmos/Events/ApplicationEvent.h"
 
 namespace Cosmos
 {
@@ -13,7 +14,10 @@ namespace Cosmos
 
 		void Run();
 
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent&);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
